d7ef6e649beea8e123cdbb9fb279c25f02b8a3eb
Kenneth Grotdal <kenneth@grotdal.no>
2023-08-30 08:56:31 +0200

    Fixed columns on partner logos

--- footer.php
+++ footer.php
@@ -7,22 +7,22 @@ if (!is_countable($logos) || count($logos) == 0) {
 
 ?>
 
-<div class="block list-logos">
-    <div class="row">
-        <?php foreach ($logos as $logo) { ?>
-            <?php if (!empty($logo["link"])) { ?>
-                <div class="col-4 col-md-2 single-logo">
-                    <a href="<?= $logo["link"]["url"] ?>" style="background-image: url('<?= $logo["image"]["sizes"]["large"] ?>');"></a>
-                </div>
+    <div class="block list-logos">
+        <div class="row">
+            <?php foreach ($logos as $logo) { ?>
+                <?php if (!empty($logo["link"])) { ?>
+                    <div class="col-6 col-md-4 col-lg-2 single-logo">
+                        <a href="<?= $logo["link"]["url"] ?>" style="background-image: url('<?= $logo["image"]["sizes"]["large"] ?>');"></a>
+                    </div>
+                <?php } ?>
             <?php } ?>
-        <?php } ?>
+        </div>
     </div>
-</div>
 
 <?php } ?>
 
 </div>
-    </div>
+</div>
 </div>
 
 <footer>
@@ -32,13 +32,13 @@ if (!is_countable($logos) || count($logos) == 0) {
         <div class="row">
             <div class="col-12 col-md-4 footer-logo" style="background-image: url('<?= get_field("footer-image", "options")["sizes"]["large"] ?>');"></div>
 
-         
+
             <div class="col-12 col-md-4 middle-image" style="background-image: url('<?= get_field("footer-image-middle", "options")["sizes"]["large"] ?>');">
                 <?php if (!empty(get_field("link-middle-image", "options"))) { ?>
-                    <a href="<?= get_field("link-middle-image", "options")["url"] ?>"></a>    
+                    <a href="<?= get_field("link-middle-image", "options")["url"] ?>"></a>
                 <?php } ?>
             </div>
-            
+
 
             <div class="col-12 col-md-4 socialmedia">
                 <?= get_field("footer-text-right", "options") ?>
@@ -46,7 +46,7 @@ if (!is_countable($logos) || count($logos) == 0) {
                     <?php $socialMedia = get_field("social-media", "options") ?>
                     <ul>
                         <?php foreach ($socialMedia as $sm) { ?>
-                            <li  style="background-image: url('<?= $sm["image"]["sizes"]["large"] ?>');"><a href="<?= $sm["link"]["url"] ?>"></a></li>
+                            <li style="background-image: url('<?= $sm["image"]["sizes"]["large"] ?>');"><a href="<?= $sm["link"]["url"] ?>"></a></li>
                         <?php } ?>
                     </ul>
                 <?php } ?>
@@ -59,4 +59,5 @@ if (!is_countable($logos) || count($logos) == 0) {
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
